58bd18b8e538ffd91acae7149b1090f0f4fa357a
Patrick Rhead <patrick@socialdesignhouse.com>
2014-03-18 15:14:05 -0400

    Changes from last friday's hive

--- wp-content/themes/eddiemachado-bones-64ea689/functions.php
+++ wp-content/themes/eddiemachado-bones-64ea689/functions.php
@@ -45,11 +45,11 @@ require_once( 'library/custom-post-type.php' ); // you can disable this if you l
 */
 // require_once( 'library/translation/translation.php' ); // this comes turned off by default
 
-/************* THUMBNAIL SIZE OPTIONS *************/
+	/************* THUMBNAIL SIZE OPTIONS *************/
 
 // Thumbnail sizes
-add_image_size( 'bones-thumb-600', 600, 150, true );
-add_image_size( 'bones-thumb-300', 300, 100, true );
+	add_image_size( 'bones-thumb-600', 600, 150, true );
+	add_image_size( 'bones-thumb-300', 300, 100, true );
 
 /*
 to add more sizes, simply copy a line from above
@@ -74,17 +74,17 @@ you like. Enjoy!
 add_filter( 'image_size_names_choose', 'bones_custom_image_sizes' );
 
 function bones_custom_image_sizes( $sizes ) {
-    return array_merge( $sizes, array(
-        'bones-thumb-600' => __('600px by 150px'),
-        'bones-thumb-300' => __('300px by 100px'),
-    ) );
+	return array_merge( $sizes, array(
+		'bones-thumb-600' => __('600px by 150px'),
+		'bones-thumb-300' => __('300px by 100px'),
+		) );
 }
 
 /*
-The function above adds the ability to use the dropdown menu to select 
-the new images sizes you have just created from within the media manager 
-when you add media to your content blocks. If you add more image sizes, 
-duplicate one of the lines in the array and name it according to your 
+The function above adds the ability to use the dropdown menu to select
+the new images sizes you have just created from within the media manager
+when you add media to your content blocks. If you add more image sizes,
+duplicate one of the lines in the array and name it according to your
 new image size.
 */
 
@@ -100,7 +100,7 @@ function bones_register_sidebars() {
 		'after_widget' => '</div>',
 		'before_title' => '<h4 class="widgettitle">',
 		'after_title' => '</h4>',
-	));
+		));
 
 	/*
 	to add more sidebars or widgetized areas, just copy
@@ -132,7 +132,7 @@ function bones_register_sidebars() {
 
 // Comment Layout
 function bones_comments( $comment, $args, $depth ) {
-   $GLOBALS['comment'] = $comment; ?>
+	$GLOBALS['comment'] = $comment; ?>
 	<li <?php comment_class(); ?>>
 		<article id="comment-<?php comment_ID(); ?>" class="clearfix">
 			<header class="comment-author vcard">
@@ -141,19 +141,19 @@ function bones_comments( $comment, $args, $depth ) {
 					this is the new responsive optimized comment image. It used the new HTML5 data-attribute to display comment gravatars on larger screens only. What this means is that on larger posts, mobile sites don't have a ton of requests for comment images. This makes load time incredibly fast! If you'd like to change it back, just replace it with the regular wordpress gravatar call:
 					echo get_avatar($comment,$size='32',$default='<path_to_url>' );
 				*/
-				?>
-				<?php // custom gravatar call ?>
-				<?php
+					?>
+					<?php // custom gravatar call ?>
+					<?php
 					// create variable
 					$bgauthemail = get_comment_author_email();
-				?>
-				<img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5( $bgauthemail ); ?>?s=32" class="load-gravatar avatar avatar-48 photo" height="32" width="32" src="<?php echo get_template_directory_uri(); ?>/library/images/nothing.gif" />
-				<?php // end custom gravatar call ?>
-				<?php printf(__( '<cite class="fn">%s</cite>', 'bonestheme' ), get_comment_author_link()) ?>
-				<time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time(__( 'F jS, Y', 'bonestheme' )); ?> </a></time>
-				<?php edit_comment_link(__( '(Edit)', 'bonestheme' ),'  ','') ?>
-			</header>
-			<?php if ($comment->comment_approved == '0') : ?>
+					?>
+					<img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5( $bgauthemail ); ?>?s=32" class="load-gravatar avatar avatar-48 photo" height="32" width="32" src="<?php echo get_template_directory_uri(); ?>/library/images/nothing.gif" />
+					<?php // end custom gravatar call ?>
+					<?php printf(__( '<cite class="fn">%s</cite>', 'bonestheme' ), get_comment_author_link()) ?>
+					<time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time(__( 'F jS, Y', 'bonestheme' )); ?> </a></time>
+					<?php edit_comment_link(__( '(Edit)', 'bonestheme' ),'  ','') ?>
+				</header>
+				<?php if ($comment->comment_approved == '0') : ?>
 				<div class="alert alert-info">
 					<p><?php _e( 'Your comment is awaiting moderation.', 'bonestheme' ) ?></p>
 				</div>
@@ -163,8 +163,8 @@ function bones_comments( $comment, $args, $depth ) {
 			</section>
 			<?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
 		</article>
-	<?php // </li> is added by WordPress automatically ?>
-<?php
+		<?php // </li> is added by WordPress automatically ?>
+		<?php
 } // don't remove this bracket!
 
 /************* SEARCH FORM LAYOUT *****************/
@@ -182,30 +182,54 @@ function bones_wpsearch($form) {
 
 add_action( 'init', 'create_post_type' );
 function create_post_type() {
+	register_post_type( 'videos',
+		array(
+			'labels' => array(
+				'name' => __( 'Videos' ),
+				'singular_name' => __( 'Video' ),
+				'add_new' => 'Add New',
+				'add_new_item' => 'Add New Video',
+				'edit' => 'Edit',
+				'edit_item' => 'Edit Video',
+				'new_item' => 'New Video',
+				'view' => 'View',
+				'view_item' => 'View Video',
+				'search_items' => 'Search Videos',
+				'not_found' => 'No Videos found',
+				'not_found_in_trash' => 'No Videos found in Trash',
+				'parent' => 'Parent Video'
+				),
+			'public' => true,
+			'menu_position' => 15,
+			'supports' => array( 'title', 'comments', /*'editor',*/ 'thumbnail', /*'custom-fields'*/ ),
+			'has_archive' => true,
+			/*'rewrite' => array('slug' => 'resources')*/
+			)
+		);
 	register_post_type( 'resources',
 		array(
 			'labels' => array(
 				'name' => __( 'Resources' ),
 				'singular_name' => __( 'Resource' ),
 				'add_new' => 'Add New',
-                'add_new_item' => 'Add New Resource',
-                'edit' => 'Edit',
-                'edit_item' => 'Edit Resource',
-                'new_item' => 'New Resource',
-                'view' => 'View',
-                'view_item' => 'View Resource',
-                'search_items' => 'Search Resources',
-                'not_found' => 'No Resources found',
-                'not_found_in_trash' => 'No Resources found in Trash',
-                'parent' => 'Parent Resource'
-			),
-		'public' => true,
-		'menu_position' => 15,
-        'supports' => array( 'title', 'comments', /*'editor',*/ 'thumbnail', /*'custom-fields'*/ ),
-		'has_archive' => true,
-		/*'rewrite' => array('slug' => 'resources')*/
-		)
-	);
+				'add_new_item' => 'Add New Resource',
+				'edit' => 'Edit',
+				'edit_item' => 'Edit Resource',
+				'new_item' => 'New Resource',
+				'view' => 'View',
+				'view_item' => 'View Resource',
+				'search_items' => 'Search Resources',
+				'not_found' => 'No Resources found',
+				'not_found_in_trash' => 'No Resources found in Trash',
+				'parent' => 'Parent Resource'
+				),
+			'public' => true,
+			'menu_position' => 15,
+			'supports' => array( 'title', 'comments', /*'editor',*/ 'thumbnail', /*'custom-fields'*/ ),
+			'has_archive' => true,
+			/*'rewrite' => array('slug' => 'resources')*/
+			)
+		);
 }
 
 
