56e8f29659d92090047bbdf9fd05f861bff56d2f
Patrick Rhead <patrick@socialdesignhouse.com>
2014-04-01 19:35:05 -0400

    index.php changes

--- wp-content/themes/eddiemachado-bones-64ea689/index.php
+++ wp-content/themes/eddiemachado-bones-64ea689/index.php
@@ -1,72 +1,29 @@
 <?php get_header(); ?>
 
-			<div id="content">
 
-				<div id="inner-content" class="wrap clearfix">
+<div id="content">
 
-						<div id="main" class="eightcol first clearfix" role="main">
+	<div id="inner-content" class="wrap clearfix">
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<div id="main" class="twelvecol first clearfix" role="main">
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
+			<?php echo do_shortcode("[metaslider id=84]"); a?>
 
-								<header class="article-header">
+			<section class="entry-content clearfix" itemprop="articleBody">
+				<?php the_content(); ?>
+				<!-- Place somewhere in the <body> of your page -->
+			</section>
 
-									<h1 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-									<p class="byline vcard"><?php
-										printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', '));
-									?></p>
+			<footer class="article-footer">
+			</footer>
 
-								</header>
 
-								<section class="entry-content clearfix">
-									<?php the_content(); ?>
-								</section>
+		</article>
 
-								<footer class="article-footer">
-									<p class="tags"><?php the_tags( '<span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '' ); ?></p>
+	</div>
 
-								</footer>
+</div>
 
-								<?php // comments_template(); // uncomment if you want to use them ?>
-
-							</article>
-
-							<?php endwhile; ?>
-
-									<?php if ( function_exists( 'bones_page_navi' ) ) { ?>
-											<?php bones_page_navi(); ?>
-									<?php } else { ?>
-											<nav class="wp-prev-next">
-													<ul class="clearfix">
-														<li class="prev-link"><?php next_posts_link( __( '&laquo; Older Entries', 'bonestheme' )) ?></li>
-														<li class="next-link"><?php previous_posts_link( __( 'Newer Entries &raquo;', 'bonestheme' )) ?></li>
-													</ul>
-											</nav>
-									<?php } ?>
-
-							<?php else : ?>
-
-									<article id="post-not-found" class="hentry clearfix">
-											<header class="article-header">
-												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
-											<section class="entry-content">
-												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
-
-							<?php endif; ?>
-
-						</div>
-
-						<?php get_sidebar(); ?>
-
-				</div>
-
-			</div>
+</div>
 
 <?php get_footer(); ?>
