79eaa006ddf8879ed50cf947d9de40140411a914
Patrick Rhead <patrick@socialdesignhouse.com>
2014-04-25 00:56:21 -0400

    CSS styles

--- wp-content/themes/eddiemachado-bones-64ea689/page-about-us.php
+++ wp-content/themes/eddiemachado-bones-64ea689/page-about-us.php
@@ -10,53 +10,53 @@ echo "<style>#menu-item-234 a {color: #9DB363</style>" ?>
 <div id="content">
 
 	<div id="inner-content-secondary" class="wrap clearfix">
-		<div class="headerimg"><img src="http://spring2014.hiveu.me/pi/patrick/wp-content/uploads/2014/04/3.jpg" alt="test_header" width="" height="" /></div>
+		<div class="headerimg"><img src="<?php echo the_field('header_image', $post->ID) ?>" alt="" width="" height="" /></div>
 		<?php
 		include('main-cta.php');
 
 		?>
 		<div class="threecol">
-		<div id="menu">
+			<div id="menu">
 
-			<ul>
-				<li class="sub-menu-title">
-					<?php the_field('menu_title', $post->ID) ?>
-				</li>
-				<?php
-				if( have_rows('page_area') ):
-					$x = 0;
-				while ( have_rows('page_area') ) : the_row();
-				$x++;
-				?>
-				<li id="sub-menu-<?php echo $x ?>" class="sub-menu-item <?php if($x == 1) {echo "active";} else {echo "inactive";} ?>">
-					<?php the_sub_field('menu_title');  ?>
-				</li>
+				<ul>
+					<li class="sub-menu-title">
+						<?php the_field('menu_title', $post->ID) ?>
+					</li>
+					<?php
+					if( have_rows('page_area') ):
+						$x = 0;
+					while ( have_rows('page_area') ) : the_row();
+					$x++;
+					?>
+					<li id="sub-menu-<?php echo $x ?>" class="sub-menu-item <?php if($x == 1) {echo "active";} else {echo "inactive";} ?>">
+						<?php the_sub_field('menu_title');  ?>
+					</li>
 
-				<?php
-				endwhile;
-				else :
-					endif;
-				?>
-			</ul>
-		</div>
+					<?php
+					endwhile;
+					else :
+						endif;
+					?>
+				</ul>
+			</div>
 		</div>
 		<div class="ninecol">
-		<?php
+			<?php
 
-		if( have_rows('page_area') ):
-			$x = 0;
-		while ( have_rows('page_area') ) : the_row();
-		$x++;
-		?>
-		<div id="sub-content-<?php echo $x ?>" class="sub-content-item <?php if($x == 1) {echo "content-visible";} else {echo "content-hidden";} ?>">
-			<?php the_sub_field('content'); ?>
+			if( have_rows('page_area') ):
+				$x = 0;
+			while ( have_rows('page_area') ) : the_row();
+			$x++;
+			?>
+			<div id="sub-content-<?php echo $x ?>" class="sub-content-item <?php if($x == 1) {echo "content-visible";} else {echo "content-hidden";} ?>">
+				<?php the_sub_field('content'); ?>
+			</div>
+			<?php
+			endwhile;
+			else :
+				endif;
+			?>
 		</div>
-		<?php
-		endwhile;
-		else :
-			endif;
-		?>
-	</div>
 	</div>
 
 </div>
--- wp-content/themes/eddiemachado-bones-64ea689/page-get-help.php
+++ wp-content/themes/eddiemachado-bones-64ea689/page-get-help.php
@@ -10,65 +10,53 @@ echo "<style>.getheader {margin-top:-146px;}#help {background-color: #9DB363</st
 <div id="content">
 
 	<div id="inner-content-secondary" class="wrap clearfix">
-		<div class="headerimg"><img src="http://spring2014.hiveu.me/pi/patrick/wp-content/uploads/2014/04/2.jpg" alt="test_header" width="" height="" /></div>
+		<div class="headerimg"><img src="<?php echo the_field('header_image', $post->ID) ?>" alt="" width="" height="" /></div>
 		<?php
 		include('main-cta.php');
-	
+
 		?>
-		<!--
-		<div id="main" class="eightcol first clearfix" role="main">
-
-			<?php the_field('get_involved_title', $post->ID); ?>
-			<br />
-			<?php the_field('get_involved_sub_title', $post->ID); ?>
-			<br />
-			<?php the_field('get_involved_contnet', $post->ID); ?>
-
-		</div>-->
 		<div class="threecol">
-		<div id="menu">
+			<div id="menu">
 
-			<ul>
-				<li class="sub-menu-title">
-					<?php the_field('menu_title', $post->ID) ?>
-				</li>
-				<?php
-				// What I have to do now is add the fucntionalty to remove the content-hidden class on the content when the user clicks that menu item
-				// So it will check for the menu_title in the contnet, and if it matched then add the show class, THEN remove the hidden class.
-				if( have_rows('page_area') ):
-					$x = 0;
-				while ( have_rows('page_area') ) : the_row();
-				$x++;
-				?>
-				<li id="sub-menu-<?php echo $x ?>" class="sub-menu-item <?php if($x == 1) {echo "active";} else {echo "inactive";} ?>">
-					<?php the_sub_field('menu_title');  ?>
-				</li>
+				<ul>
+					<li class="sub-menu-title">
+						<?php the_field('menu_title', $post->ID) ?>
+					</li>
+					<?php
+					if( have_rows('page_area') ):
+						$x = 0;
+					while ( have_rows('page_area') ) : the_row();
+					$x++;
+					?>
+					<li id="sub-menu-<?php echo $x ?>" class="sub-menu-item <?php if($x == 1) {echo "active";} else {echo "inactive";} ?>">
+						<?php the_sub_field('menu_title');  ?>
+					</li>
 
-				<?php
-				endwhile;
-				else :
-					endif;
-				?>
-			</ul>
-		</div>
+					<?php
+					endwhile;
+					else :
+						endif;
+					?>
+				</ul>
+			</div>
 		</div>
 		<div class="ninecol">
-		<?php
+			<?php
 
-		if( have_rows('page_area') ):
-			$x = 0;
-		while ( have_rows('page_area') ) : the_row();
-		$x++;
-		?>
-		<div id="sub-content-<?php echo $x ?>" class="sub-content-item <?php if($x == 1) {echo "content-visible";} else {echo "content-hidden";} ?>">
-			<?php the_sub_field('content'); ?>
+			if( have_rows('page_area') ):
+				$x = 0;
+			while ( have_rows('page_area') ) : the_row();
+			$x++;
+			?>
+			<div id="sub-content-<?php echo $x ?>" class="sub-content-item <?php if($x == 1) {echo "content-visible";} else {echo "content-hidden";} ?>">
+				<?php the_sub_field('content'); ?>
+			</div>
+			<?php
+			endwhile;
+			else :
+				endif;
+			?>
 		</div>
-		<?php
-		endwhile;
-		else :
-			endif;
-		?>
-	</div>
 	</div>
 
 </div>
